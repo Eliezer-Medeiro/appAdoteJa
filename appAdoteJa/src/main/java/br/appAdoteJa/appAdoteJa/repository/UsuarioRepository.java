e34eb8daa4998c062e128f06d6197fc310b8152d
Eliezer-Medeiro <128553695+Eliezer-Medeiro@users.noreply.github.com>
2025-11-10 14:17:06 -0400

    Add method to find user by email

--- appAdoteJa/src/main/java/br/appAdoteJa/appAdoteJa/repository/UsuarioRepository.java
+++ appAdoteJa/src/main/java/br/appAdoteJa/appAdoteJa/repository/UsuarioRepository.java
@@ -11,4 +11,5 @@ public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     @Query(value="SELECT * FROM usuario WHERE email = :email AND senha = :senha", nativeQuery = true)
     public Usuario login(@Param("email") String email, @Param("senha") String senha);
 
+    Optional<Usuario> findByEmail(String email);
 }

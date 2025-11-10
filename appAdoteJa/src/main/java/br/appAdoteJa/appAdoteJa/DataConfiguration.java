952a0d06749166728fee0add876a4d799d0a289d
Eliezer <eliezer.192815@gmail.com>
2025-11-10 12:06:28 -0300

    mudei por causa do render que la é em PostgreSQL

--- appAdoteJa/src/main/java/br/appAdoteJa/appAdoteJa/DataConfiguration.java
+++ appAdoteJa/src/main/java/br/appAdoteJa/appAdoteJa/DataConfiguration.java
@@ -1,7 +1,6 @@
 package br.appAdoteJa.appAdoteJa;
 
 import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -9,28 +8,28 @@ import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-
 @Configuration
 public class DataConfiguration {
-	
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/appadoteja?useTimezone=True&serverTimezone=UTC");
-		dataSource.setUsername("root");
-		dataSource.setPassword("Eliezer154.");
-		return dataSource;
-	}
 
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(false);
-		adapter.setPrepareConnection(true);
-		return adapter;
-	}
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(System.getenv("DB_URL"));
+        dataSource.setUsername(System.getenv("DB_USER"));
+        dataSource.setPassword(System.getenv("DB_PASSWORD"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false);
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
 }

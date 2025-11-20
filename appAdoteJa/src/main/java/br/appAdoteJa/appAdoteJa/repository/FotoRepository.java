7ed4f074a27c673ba9e7179a0f406e89be719112
Eliezer-Medeiro <128553695+Eliezer-Medeiro@users.noreply.github.com>
2025-11-20 10:12:42 -0300

    Create FotoRepository.java

---
+++ appAdoteJa/src/main/java/br/appAdoteJa/appAdoteJa/repository/FotoRepository.java
@@ -0,0 +1,9 @@
+package br.appAdoteJa.appAdoteJa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.appAdoteJa.appAdoteJa.model.Foto;
+
+public interface FotoRepository extends JpaRepository<Foto, Long> {
+
+}
